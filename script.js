b9591613b41ccc83e0c19e9e09098ed56addc3cb
- <srconvidadotrl120@gmail.com>
2026-08-24 11:31:55 -0300

    Varius Small Changes

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.querySelector('form').addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
-    event.preventDefault(); // Impede o envio padrão do formulário
+    event.preventDefault(); 
 
     const nome = document.querySelector('input[name="Nome"]').value;
-    const memoria = document.querySelector('input[name="Memória"]').value;
+    const memoria = document.querySelector('input[name="Memoria"]').value;
     const created = new Date().toISOString();
 
     document.querySelector("form button[type='submit']").disabled = true;
@@ -35,7 +35,6 @@ function iniciarLoadingAscii(elementoBotao) {
     
     let etapaAtual = 0;
 
-    // Atualiza o texto do botão a cada 300 milissegundos
     const intervalo = setInterval(() => {
         if (etapaAtual < etapasBarra.length) {
             elementoBotao.textContent = etapasBarra[etapaAtual];
